0c911cf3569170924fc39bfd5e9f3126db07a7e7
Hattou Mehdi <102341310+Teczer@users.noreply.github.com>
2025-06-29 16:07:13 +0200

    Updated collection-detail.tsx

--- components/collection-detail/collection-detail.tsx
+++ components/collection-detail/collection-detail.tsx
@@ -126,7 +126,7 @@ export function CollectionDetail({ collection: initialCollection, onBack, onPrac
                 />
 
                 <span class="relative flex size-3">
-  <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+  <span class="absolute inline-flex h-full w-full animate-ping rounded-full opacity-75"></span>
   <span class="relative inline-flex size-3 rounded-full"
     
         style={{ backgroundColor: currentCollection.color }}
